ffdd37199d932e894ad8d2955b704f8a57420f82
amiro hatami <amirohatami@amiros-Laptop.local>
2024-06-14 09:45:03 +0330

    Finilize the app for usning, polosh the app component, Jun 14 , 2024

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import Player from "./components/Player";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 import GameOver from "./components/GameOver";
-
-const initialGameBoard = [
+const PLAYERS = {
+  X : 'Player 1' ,
+  O : 'Player 2',
+}
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -19,18 +22,18 @@ function derivedActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-function App() {
-  const [gameTurns, setGameTurns] = useState([]);
-  const activePlayer = derivedActivePlayer(gameTurns);
-
-  let gameBoard = [...initialGameBoard.map(array => [...array])];
+function derivedGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD.map((array) => [...array])];
 
   for (const turn of gameTurns) {
     const { square, Player } = turn;
     const { row, col } = square;
     gameBoard[row][col] = Player;
   }
+  return gameBoard;
+}
 
+function deriveWinner(gameBoard, players) {
   let winner = null;
 
   for (const combination of WINNING_COMBINATIONS) {
@@ -45,10 +48,19 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
+  return winner;
+}
 
+function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  const activePlayer = derivedActivePlayer(gameTurns);
+  const [players, setPlayers] = useState(PLAYERS);
+
+  const gameBoard = derivedGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -63,24 +75,36 @@ function App() {
   }
 
   function handleRestart() {
-    setGameTurns([])
+    setGameTurns([]);
+  }
+  function handlePalyerNameChange(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName,
+      };
+    });
   }
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
           <Player
-            initialName="Player 1"
+            initialName={PLAYERS.X}
             symbol="X"
             isActive={activePlayer === "X"}
+            onChanageName={handlePalyerNameChange}
           />
           <Player
-            initialName="Player 2"
+            initialName={PLAYERS.O}
             symbol="O"
             isActive={activePlayer === "O"}
+            onChanageName={handlePalyerNameChange}
           />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} onReset={handleRestart} />}
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onReset={handleRestart} />
+        )}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
-export default function Player({ initialName, symbol, isActive}) {
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChanageName,
+}) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEdititng, setIsEditing] = useState(false);
   function handleEditClick() {
     setIsEditing((editing) => !editing);
+    if (isEdititng) {
+      onChanageName(symbol, playerName);
+    }
   }
   function handleChange(event) {
     console.log(event);
-    console.log("Amiroo")
     setPlayerName(event.target.value);
   }
 
   //   let btnCaption = "Edit"
-  // new 
+  // new
   let editablePlayerName = <span className="player-name">{playerName}</span>;
   if (isEdititng) {
     editablePlayerName = (
@@ -22,7 +29,7 @@ export default function Player({ initialName, symbol, isActive}) {
   }
 
   return (
-    <li className={isActive ? 'active' : undefined}>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
